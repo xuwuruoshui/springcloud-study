7d43b3ec69dfcd8e22035e444da31b62a6cee5ba
xuwuruoshui <1085252985@qq.com>
2020-03-28 16:53:19 +0800

    hahah

--- order-service/src/main/java/top/xuwuruoshui/orderservice/service/impl/ProductOrderServiceImpl.java
+++ order-service/src/main/java/top/xuwuruoshui/orderservice/service/impl/ProductOrderServiceImpl.java
@@ -1,12 +1,15 @@
 package top.xuwuruoshui.orderservice.service.impl;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import top.xuwuruoshui.orderservice.domain.ProductOrder;
+import top.xuwuruoshui.orderservice.service.ProductClient;
 import top.xuwuruoshui.orderservice.service.ProductOrderService;
+import top.xuwuruoshui.orderservice.util.JsonUtils;
 
 import java.net.URI;
 import java.util.Date;
@@ -16,32 +19,24 @@ import java.util.UUID;
 @Service
 public class ProductOrderServiceImpl implements ProductOrderService {
 
-    private RestTemplate restTemplate;
-
-    public ProductOrderServiceImpl(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
-/*    @Autowired
-    private LoadBalancerClient loadBalancer;*/
+    @Autowired
+    private ProductClient productClient;
 
     @Override
     public ProductOrder save(int userId, int productId) {
 
-        Map<String,Object> map = restTemplate.getForObject("http://product-service/api/v1/product/find?id=" + productId, Map.class);
+        String response = productClient.findById(productId);
+        JsonNode jsonNode = JsonUtils.str2JsonNode(response);
 
-/*        ServiceInstance instance = loadBalancer.choose("product-service");
-        URI storesUri = URI.create(String.format("http://%s:%s/api/v1/product/find?id="+productId, instance.getHost(), instance.getPort()));
-        Map<String,Object> map = restTemplate.getForObject(storesUri, Map.class);*/
 
-        System.out.println(map);
+
         //获取商品详情
         ProductOrder productOrder = new ProductOrder();
         productOrder.setCreateTime(new Date());
         productOrder.setUserId(userId);
         productOrder.setTradeNo(UUID.randomUUID().toString());
-        productOrder.setProductName(map.get("name").toString());
-        productOrder.setPrice((Integer) map.get("price"));
+        productOrder.setProductName(jsonNode.get("name").toString());
+        productOrder.setPrice(Integer.valueOf(jsonNode.get("price").toString()));
 
         return productOrder;
     }
