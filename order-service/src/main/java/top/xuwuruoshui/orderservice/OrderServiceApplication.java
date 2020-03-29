79c0ce1a7664c3f87b503ef6a705252c47c9d100
xuwuruoshui <1085252985@qq.com>
2020-03-29 17:20:57 +0800

    test git

--- order-service/src/main/java/top/xuwuruoshui/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/top/xuwuruoshui/orderservice/OrderServiceApplication.java
@@ -3,7 +3,6 @@ package top.xuwuruoshui.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
