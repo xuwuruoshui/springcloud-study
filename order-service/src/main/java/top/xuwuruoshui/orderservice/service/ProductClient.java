ae8940dc79713510f39b682be78a1526f089094c
xuwuruoshui <1085252985@qq.com>
2020-03-29 20:23:06 +0800

    catch the hystrix error

--- order-service/src/main/java/top/xuwuruoshui/orderservice/service/ProductClient.java
+++ order-service/src/main/java/top/xuwuruoshui/orderservice/service/ProductClient.java
@@ -4,11 +4,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import top.xuwuruoshui.orderservice.fallback.ProductClientFallback;
 
 /**
  * 商品服务客户端
  */
-@FeignClient("product-service")
+@FeignClient(value = "product-service",fallback = ProductClientFallback.class)
 public interface ProductClient {
     @GetMapping("/api/v1/product/find")
     String findById(@RequestParam(value ="id")int id);
