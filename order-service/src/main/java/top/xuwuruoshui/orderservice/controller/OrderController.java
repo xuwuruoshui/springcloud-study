1c74c8e0d049ae20e5bc5a479fb3d9cbb77e6c5b
xuwuruoshui <1085252985@qq.com>
2020-04-18 23:36:10 +0800

    fix redis

--- order-service/src/main/java/top/xuwuruoshui/orderservice/controller/OrderController.java
+++ order-service/src/main/java/top/xuwuruoshui/orderservice/controller/OrderController.java
@@ -5,7 +5,6 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,7 +40,7 @@ public class OrderController {
     private ResponseEntity<?> saveOrderFail(int userId, int productId, HttpServletRequest request){
         //monitor
         String saveOrderKey = "save-order";
-        String sendValue = redisTemplate.opsForValue().get("saveOrderKey");
+        String sendValue = redisTemplate.opsForValue().get(saveOrderKey);
         final String ipAddress = request.getRemoteAddr();
 
         new Thread(()->{
